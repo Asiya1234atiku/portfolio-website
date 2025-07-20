9250a80b1dc4102e90300ec5ab30b4e4e9db4ee6
Asiya1234atiku <atikuasiya81@gmail.com>
2025-07-20 09:08:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple welcome alert
+window.addEventListener('load', function () {
+  if (window.location.pathname.includes("index.html")) {
+    alert("Welcome to Asiya Atiku’s Portfolio Site 💙");
+  }
+});
